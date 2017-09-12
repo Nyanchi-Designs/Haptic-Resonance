3a3bf5438287d20cf3845a74775a202d8c0295ba
James Johnson <music1384@yahoo.co.uk>
2017-09-12 17:56:32 +0100

    Fix 2D scene, add collectable init

--- Assets/Code/Classes/Collectable.cs
+++ Assets/Code/Classes/Collectable.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Collider2D))]
 public abstract class Collectable : MonoBehaviour
 {
     [Tooltip ("How much is the player awarded upon collecting this.")]
     [SerializeField] protected int _Score = 0;
+
+    private void Awake()
+    {
+        GetComponent<Collider2D> ().isTrigger = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
