444fe1bf6125ee0bfdeaab103f79b765aace890f
James Johnson <music1384@yahoo.co.uk>
2017-09-12 16:25:02 +0100

    Remove update dependency.

--- Assets/Code/Classes/CameraController.cs
+++ Assets/Code/Classes/CameraController.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 //TODO: Think of better name for scroll speed.
-//TODO: Move coroutine out of update loop as is a waste.
-//TODO: Consider making the entire scrolling a corouting and move it out of update.
 public class CameraController : MonoBehaviour
 {
     [Tooltip ("The speed of the camera's movement.")]
     [SerializeField] private float _ScrollSpeed = 5f;
-    public int RoutinesRunning = 0;
 
-    private bool _CanMove = false;
-    private bool _IsRunning = false;
     private Transform _Transform = null;
 
     private void Awake()
@@ -20,47 +15,36 @@ public class CameraController : MonoBehaviour
         EventManager.OnObstacleHit += ObstacleHit;
     }
 
-    private void Update()
+    private IEnumerator MoveTo (Vector3 position, float speed)
     {
-        // If an obstacle was hit and the camera can move, start scrolling.
-        if (_CanMove)
-        {
-            _Transform.Translate (_Transform.right * _ScrollSpeed * Time.fixedDeltaTime);
-            StopAllCoroutines ();
-            _IsRunning = false; 
-        }
-        // If no obstacle is hit, then start moving the camera back to the centre of the screen.
-        else if (!_CanMove && _Transform.position.x >= 0.05f && !_IsRunning)
-            StartCoroutine (MoveTo (new Vector3 (0.0f, _Transform.position.y, _Transform.position.z)));
-    }
-
-    private IEnumerator MoveTo (Vector3 position)
-    {
-        _IsRunning = true;
         var startPos = _Transform.position;
         var elapsedTime = 0.0f;
 
-        while (elapsedTime < 5f)
+        while (elapsedTime < speed)
         {
-            _Transform.position = Vector3.Lerp (startPos, position, elapsedTime / 5f);
-            elapsedTime += Time.deltaTime;
+            _Transform.position = Vector3.Lerp (startPos, position, elapsedTime / speed);
+            elapsedTime += Time.fixedDeltaTime;
 
             yield return new WaitForEndOfFrame ();
         }
 
         StopAllCoroutines ();
-        _IsRunning = false;
     }
 
     private void ObstacleHit (bool hit)
     {
         if (hit)
-            _CanMove = true;
+        {
+            // Scroll to the right of the screen.
+            StopAllCoroutines ();
+            StartCoroutine (MoveTo (new Vector3 (15.0f, _Transform.position.y, _Transform.position.z), _ScrollSpeed));
+        }
         else
-            _CanMove = false;
+            // Scroll back to the center area of the screen.
+            StartCoroutine (MoveTo (new Vector3 (0.0f, _Transform.position.y, _Transform.position.z), 5f));
     }
 
-    private void OnDestroy()
+    private void OnDestroy ()
     {
         EventManager.OnObstacleHit -= ObstacleHit;
     }
